a3d179749ad8aeb6de57b3f41e6209541f30d82e
mdnazmul42726 <nazmulofficial@outlook.com>
2023-10-30 20:19:38 +0600

    Initialized react router, tailwind css, firebase

---
+++ src/firebase.init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBFy-n2CCLQDpASZFIaT3f1-P8Ifd10Tto",
+    authDomain: "simple-52.firebaseapp.com",
+    projectId: "simple-52",
+    storageBucket: "simple-52.appspot.com",
+    messagingSenderId: "289854559475",
+    appId: "1:289854559475:web:01375111a0e046eff09386"
+};
+
+const app = initializeApp(firebaseConfig);
